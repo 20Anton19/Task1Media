0c1c0ad30f0f4106a3594c54fa72fd2d723359bf
20Anton19 <lebedew.anton2018@yandex.ru>
2025-06-26 15:23:07 +0400

    Добавил проверку ToString

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         BankAccount account1 = new BankAccount("Антон Лебедев");
         BankAccount account2 = new BankAccount("Михаил Осипов");
 
+        System.out.println("Счет 1: " + account1);
+        System.out.println("Счет 2: " + account2);
+
+
         boolean depositAccount = account1.Deposit(500);
         System.out.println("Пополнение счета: " + (depositAccount ? "Успешно" : "Неудача"));
         System.out.println("Счет: " + account1.GetBalance());
