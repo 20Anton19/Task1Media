8ad0a151cbc6f90463e60ead32747679f11cc7a2
20Anton19 <lebedew.anton2018@yandex.ru>
2025-06-26 14:47:14 +0400

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime;
+ class BankAccount {
+    private String ownerName;
+
+    private int balance;
+
+    private LocalDateTime openingDate;
+
+    private Boolean isBlocked;
+
+    public BankAccount(String ownerName) {
+        this.ownerName = ownerName;
+        balance = 500000;
+        openingDate = LocalDateTime.of(2025, 6, 25, 14, 30);
+        isBlocked = false;
+    }
+
+    public boolean Deposit(int amount){
+        balance += amount;
+        return true;
+    }
+
+     public boolean Withdraw(int amount){
+         if (balance >= amount) {
+             balance -= amount;
+             return true;
+         }
+         else {
+             System.out.println("Ошибка! Недостаточно средств.");
+             return false;
+         }
+     }
+
+     public boolean transfer(BankAccount otherAccount, int amount) {
+         if (balance >= amount) {
+             balance -= amount;
+             otherAccount.balance += amount;
+             return true;
+         }
+         else {
+             System.out.println("Ошибка! Недостаточно средств.");
+             return false;
+         }
+     }
+}
